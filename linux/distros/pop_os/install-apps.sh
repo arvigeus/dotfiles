3b62319b9bfe7b8d038ee3a170c68ca6afc12dfa
Nikolay Stoynov <arvigeus@gmail.com>
2020-07-01 07:42:22 +0300

    Tweak install scripts

--- linux/distros/pop_os/install-apps.sh
+++ linux/distros/pop_os/install-apps.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-flatpak install com.getpostman.Postman
-flatpak install com.obsproject.Studio
-flatpak install com.slack.Slack
-flatpak install org.gimp.GIMP
-flatpak install org.gnome.meld
-flatpak install org.qbittorrent.qBittorrent
-flatpak install org.videolan.VLC
-flatpak install org.inkscape.Inkscape
-flatpak install org.audacityteam.Audacity
+sudo apt -y dist-upgrade
 
-sudo apt install chromium mpv steam glhack nodejs npm wget youtube-dl lutris winetricks gamemode p7zip-full libnotify-bin nautilus-admin
+flatpak install -y com.obsproject.Studio
+flatpak install -y com.slack.Slack
+flatpak install -y org.gimp.GIMP
+flatpak install -y org.gnome.meld
+flatpak install -y org.qbittorrent.qBittorrent
+flatpak install -y org.videolan.VLC
+flatpak install -y org.inkscape.Inkscape
+flatpak install -y org.audacityteam.Audacity
+
+sudo apt -y install chromium code mpv steam glhack nodejs npm wget youtube-dl lutris winetricks gamemode p7zip-full libnotify-bin glhack
 
 # Open with VSCode
 wget -qO- https://raw.githubusercontent.com/cra0zy/code-nautilus/master/install.sh | bash
@@ -19,5 +20,7 @@ wget -qO- https://raw.githubusercontent.com/cra0zy/code-nautilus/master/install.
 # https://support.insomnia.rest/article/23-installation#linux
 echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install insomnia
+sudo apt-get -y update
+sudo apt-get -y install insomnia
+
+sudo apt -y autoremove
